28e99124ca92bac3114297ecbd9193ebb9790317
Asiful Islam Sakib <sakib.coder@gmail.com>
2023-09-17 00:17:01 +0600

    (main) fix: Add extra spaces

--- helpers/chunks.js
+++ helpers/chunks.js
@@ -1,8 +1,10 @@
 export const sliceIntoChunks = (arr, chunkSize) => {
   const res = []
+
   for (let i = 0; i < arr.length; i += chunkSize) {
     const chunk = arr.slice(i, i + chunkSize)
     res.push(chunk)
   }
+
   return res
 }
