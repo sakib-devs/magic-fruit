c451418a79af24de44144d64a00ec34997712f2c
Asiful Islam Sakib <sakib.coder@gmail.com>
2023-09-24 23:04:01 +0600

    (main) fix: Design last page

--- components/Main.vue
+++ components/Main.vue
@@ -4,28 +4,38 @@
       v-if="isBeforeGameStarted"
       class="h-96 flex flex-col justify-center items-center">
       <span
-        class="text-3xl font-bold hover:scale-125 duration-500 hover:text-emerald-500"
+        class="text-[3rem] font-bold hover:scale-125 duration-500 hover:text-emerald-500"
         v-text="textGreetings" />
       <PlayIcon
         class="play-icon hover:scale-125 duration-500 text-cyan-500 px-2 rounded-full cursor-pointer"
         :title="$t('general.play')"
         @click="startGame" />
+      <button
+        class="font-semibold"
+        v-text="$t('general.play')"
+        @click="startGame" />
     </div>
 
     <div v-if="isGameStarted" class="flex flex-col justify-center items-center">
-      <div class="flex justify-center items-start gap-12 h-96">
+      <div class="my-4 flex justify-center items-start gap-12 h-96">
         <ul
           class="flex flex-col gap-1"
           v-for="(fruits, index) in chunkedFruits"
           :key="`chuncked-fruit-${index}`">
-          <li v-for="{ id, name } in fruits" :key="id" v-text="name" />
+          <li
+            class="text-lg font-semibold hover:text-cyan-600 duration-500 cursor-pointer"
+            v-for="{ id, name } in fruits"
+            :key="id"
+            v-text="name" />
         </ul>
       </div>
 
       <div
         v-if="isStarterRound"
         class="flex flex-col justify-center items-center">
-        <div v-text="$t('fruit.pick-fruit')" />
+        <div
+          class="my-2 text-lg text-emerald-700 font-bold"
+          v-text="$t('fruit.pick-fruit')" />
         <div class="my-2 flex gap-2">
           <button
             class="w-28 py-1 bg-pink-700 hover:bg-pink-800 text-white rounded hover:font-semibold duration-300"
@@ -41,7 +51,9 @@
       <div
         v-if="isPlayingRound"
         class="flex flex-col justify-center items-center">
-        <div v-text="$t('fruit.confirmation')" />
+        <div
+          class="my-2 text-lg text-cyan-700 font-bold"
+          v-text="$t('fruit.confirmation')" />
         <div class="my-2 flex gap-2">
           <button
             class="w-28 py-1 bg-rose-700 text-white rounded hover:bg-rose-800 hover:font-semibold duration-300"
@@ -57,22 +69,31 @@
 
     <div
       v-if="isGameFinished"
-      class="flex flex-col justify-center items-center">
+      class="h-96 flex flex-col justify-center items-center">
       <div class="flex flex-col justify-center items-center">
         <span
           v-if="hasAnswer"
           class="text-3xl font-bold"
           v-text="$t('general.your-answer')" />
-        <span class="text-3xl font-bold" v-text="pickedFruit" />
-        <span v-if="hasAnswer" v-text="wrongAnswer" />
+        <span
+          class="mt-4 text-emerald-500 text-[4rem] font-bold"
+          v-text="pickedFruit" />
+        <span class="mt-1" v-if="hasAnswer" v-text="wrongAnswer" />
       </div>
-      <button v-text="$t('general.play-again')" @click="startGame" />
+      <PlayIcon
+        class="mt-2 play-icon hover:scale-125 duration-500 text-cyan-500 px-2 rounded-full cursor-pointer"
+        :title="$t('general.play')"
+        @click="startGame" />
+      <button
+        class="font-semibold"
+        v-text="$t('general.play-again')"
+        @click="startGame" />
     </div>
   </div>
 </template>
 
-<script setup>
 import { computed, ref } from 'vue'
+<script setup>
 import { useI18n } from 'vue-i18n'
 import { fruitsMap, answer } from '@/utils/fruits'
 import { sliceIntoChunks } from '@/helpers/chunks'
@@ -153,7 +174,7 @@ function decreaseRound() {
 
 <style scoped>
 .play-icon {
-  height: 4rem;
-  width: 4rem;
+  height: 5rem;
+  width: 5rem;
 }
 </style>
