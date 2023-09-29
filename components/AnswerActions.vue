521076f5c1ae82c8ff8cb0102bbc26e4d0cff22c
Asiful Islam Sakib <sakib.coder@gmail.com>
2023-09-29 18:44:20 +0600

    (main) chore: Add icons for action buttons

--- components/AnswerActions.vue
+++ components/AnswerActions.vue
@@ -4,22 +4,30 @@
       class="mt-6 mb-2 text-center text-lg text-cyan-700 font-bold"
       v-text="confirmationMessage" />
 
-    <div class="my-2 flex gap-3">
+    <div class="my-2 flex gap-4">
       <button
-        class="w-28 py-1 bg-rose-700 text-white rounded hover:bg-rose-800 hover:font-semibold hover:scale-110 duration-500"
-        v-text="$t('general.no')"
-        @click="$emit('increase', 'no')" />
+        class="flex gap-2 justify-center items-center w-32 py-1.5 bg-rose-700 text-white rounded hover:bg-rose-800 hover:font-semibold hover:scale-110 duration-500"
+        @click="$emit('increase', 'no')">
+        <XIcon
+          class="action-icon mt-[0.15rem] rounded-full text-rose-700 hover:text-rose-800 font-bold bg-white" />
+        <span v-text="$t('general.no')" />
+      </button>
 
       <button
-        class="w-28 py-1 bg-emerald-700 text-white rounded hover:bg-emerald-800 hover:font-semibold hover:scale-110 duration-500"
-        v-text="$t('general.yes')"
-        @click="$emit('increase', 'yes')" />
+        class="flex gap-2 justify-center items-center w-32 py-1 bg-emerald-700 text-white rounded hover:bg-emerald-800 hover:font-semibold hover:scale-110 duration-500"
+        @click="$emit('increase', 'yes')">
+        <span v-text="$t('general.yes')" />
+        <CheckIcon
+          class="action-icon mt-[0.15rem] rounded-full text-emerald-700 hover:text-emerald-800 font-bold bg-white" />
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n'
+import XIcon from '@/assets/icons/x.svg'
+import CheckIcon from '@/assets/icons/check.svg'
 
 const props = defineProps({
   currentRound: Number,
--- components/IncreamentActions.vue
+++ components/IncreamentActions.vue
@@ -4,20 +4,29 @@
       class="mt-6 mb-2 text-center text-lg text-emerald-700 font-bold"
       v-text="$t('fruit.pick-fruit')" />
 
-    <div class="my-2 flex gap-3">
+    <div class="my-2 flex gap-4">
       <button
-        class="w-28 py-1 bg-pink-700 hover:bg-pink-800 text-white rounded hover:font-semibold hover:scale-110 duration-500"
-        @click="$emit('decrease')"
-        v-text="$t('general.previous')" />
+        class="flex gap-2 justify-center items-center w-32 py-1.5 bg-pink-700 text-white rounded hover:bg-pink-800 hover:font-semibold hover:scale-110 duration-500"
+        @click="$emit('decrease')">
+        <ArrowLeftIcon
+          class="action-icon mt-[0.15rem] rounded-full text-pink-700 hover:text-pink-800 font-bold bg-white" />
+        <span v-text="$t('general.previous')" />
+      </button>
 
       <button
-        class="w-28 py-1 bg-cyan-700 hover:bg-cyan-800 text-white rounded hover:font-semibold hover:scale-110 duration-500"
-        @click="$emit('increase', 'next')"
-        v-text="$t('general.next')" />
+        class="flex gap-2 justify-center items-center w-32 py-1.5 bg-cyan-700 text-white rounded hover:bg-cyan-800 hover:font-semibold hover:scale-110 duration-500"
+        @click="$emit('increase', 'next')">
+        <span v-text="$t('general.next')" />
+        <ArrowRightIcon
+          class="action-icon mt-[0.15rem] rounded-full text-cyan-700 hover:text-cyan-800 font-bold bg-white" />
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import ArrowLeftIcon from '@/assets/icons/arrow-left.svg'
+import ArrowRightIcon from '@/assets/icons/arrow-right.svg'
+
 const emit = defineEmits(['increase', 'decrease'])
 </script>
