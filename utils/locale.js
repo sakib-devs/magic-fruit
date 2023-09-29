80a5f4fbceca06e831c7c657b9791ca322efc193
Asiful Islam Sakib <sakib.coder@gmail.com>
2023-09-29 17:04:34 +0600

    (main) fix: Set 'Bangla' as first language

--- utils/locale.js
+++ utils/locale.js
@@ -1,7 +1,7 @@
-export const EN = { code: 'en', name: 'English', file: 'en.json' }
 export const BN = { code: 'bn', name: 'বাংলা', file: 'bn.json' }
+export const EN = { code: 'en', name: 'English', file: 'en.json' }
 export const ES = { code: 'es', name: 'Español', file: 'es.json' }
 export const FR = { code: 'fr', name: 'Français', file: 'fr.json' }
 export const DE = { code: 'de', name: 'Deutsch', file: 'de.json' }
 
-export default [EN, BN, ES, FR, DE]
+export default [BN, EN, ES, FR, DE]
