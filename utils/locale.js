acb41c46b2cea232bb1e6d81b867af5c3cf132ca
Asiful Islam Sakib <sakib.coder@gmail.com>
2023-10-01 22:07:47 +0600

    (main) feat: Add i18n for 'Hindi' and 'Italian' language

--- utils/locale.js
+++ utils/locale.js
@@ -1,7 +1,9 @@
 export const BN = { code: 'bn', name: 'বাংলা', file: 'bn.json' }
+export const HI = { code: 'hi', name: 'हिन्दी', file: 'hi.json' }
 export const EN = { code: 'en', name: 'English', file: 'en.json' }
 export const ES = { code: 'es', name: 'Español', file: 'es.json' }
 export const FR = { code: 'fr', name: 'Français', file: 'fr.json' }
 export const DE = { code: 'de', name: 'Deutsch', file: 'de.json' }
+export const IT = { code: 'it', name: 'Italiano', file: 'it.json' }
 
-export default [BN, EN, ES, FR, DE]
+export default [BN, HI, EN, ES, FR, DE, IT]
